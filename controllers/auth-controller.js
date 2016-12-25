11894aee499c7c60169ec7c8860c7892bf466067
Keesh.Zhang <chunhui2001@gmail.com>
2016-12-25 12:56:07 +0800

    fixed a bug about auth-controller

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -6,7 +6,6 @@ passport.use(new passportLocal.Strategy(verifyCredentials));
 
 
 passport.serializeUser(function (user, done) {
-    console.log(user, 'serializeUser');
     done(null, user);
 });
 
@@ -17,9 +16,6 @@ passport.deserializeUser(function (user, done) {
 
 
 function verifyCredentials(username, password, done) {
-    console.log('verifyCredentials');       
-    console.log(username, 'username');
-    console.log(password, 'password');
     if (!username || !password) return done(true);
     if (username.length == 0 || password.length == 0) return done(true);
 
@@ -47,11 +43,8 @@ exports.authLocalCredentials = function (req, res, next) {
     }
 
     passport.authenticate('local', function (err, user, info) {
-        console.log(err, 'err');
-        console.log(user, 'user');
-        console.log(info, 'info');
         if (err) return next(err);
-        if (!user) res.redirect(loginUrl);
+        if (!user) return res.redirect(loginUrl);
         req.logIn(user, function (err) {
             if (err) return next(err);
             next();
