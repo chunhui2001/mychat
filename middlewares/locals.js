adaa76860560b138d689709f848d9e1f7fd3a0c4
Keesh.Zhang <chunhui2001@gmail.com>
2017-01-02 22:04:51 +0800

    update cinema page to add panels

--- middlewares/locals.js
+++ middlewares/locals.js
@@ -4,6 +4,6 @@ var ObjectId    = require('bson-objectid');
 module.exports = function (req, res, next) {
 	if (req.user) res.locals.user = req.user; 
 	res.locals.isAuthenticated = req.isAuthenticated();
-    res.locals.ticket = ObjectId.generate(); 
+    res.locals.socketTicket = ObjectId.generate(); 
     next();
 }
